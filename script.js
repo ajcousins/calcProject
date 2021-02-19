fe982e202f4fd0b73f141bea7908845a0e6b1299
Alvin Cousins <alvin.cousins@googlemail.com>
2021-02-19 20:06:42 +0000

    Script start

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
 const displayMain = document.querySelector(".displayMain");
 
 displayMain.textContent = "1234567890";
+
+const buttons = Array.from(document.querySelectorAll(".button"));
+
+console.log(buttons);
+
+
+
+
+
+
+function add (a, b) {
+    return a + b;
+}
+function subtract (a, b) {
+    return a - b;
+}
+function multiply (a, b) {
+    return a * b;
+}
+function divide (a, b) {
+    return a / b;
+}
