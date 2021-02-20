943d6e6f0154d3fb6101b0937e532f2410d9351b
Alvin Cousins <alvin.cousins@googlemail.com>
2021-02-20 00:11:11 +0000

    Button queries

--- script.js
+++ script.js
@@ -1,25 +1,71 @@
-const displayMain = document.querySelector(".displayMain");
-
-displayMain.textContent = "1234567890";
-
-const buttons = Array.from(document.querySelectorAll(".button"));
-
-console.log(buttons);
+var displayMain = document.querySelector(".displayMain");
 
 
 
+const buttons = document.querySelectorAll(".button");
 
 
+// buttons.forEach(function (button) {
+//     button.addEventListener("hover", console.log(button));
+// })
 
-function add (a, b) {
+// const n1 = buttons[12];
+// const n2 = buttons[13];
+// const n3 = buttons[14];
+// const n4 = buttons[8];
+// const n5 = buttons[9];
+// const n6 = buttons[10];
+// const n7 = buttons[4];
+// const n8 = buttons[5];
+// const n9 = buttons[6];
+// const n0 = buttons[16];
+
+// const clear = buttons[0];
+// const del = buttons[1];
+// const divide = buttons[2];
+// const multiply = buttons[3];
+// const minus = buttons[7];
+// const plus = buttons[11];
+// const equals = buttons[15];
+// const decimal = buttons[17];
+
+var arrayA = [0];
+
+function updateDisplay(arrayA) {
+    displayMain.textContent = arrayA.join("");
+}
+
+buttons.forEach(function (button) {
+    button.addEventListener("click", function () {
+        if (!isNaN(button.textContent) && arrayA.length < 13) {
+            if (arrayA[0] == 0) {
+                arrayA = []
+            }
+            arrayA.push(button.textContent);
+            console.log(arrayA);
+        }
+        switch (true) {
+            case button.textContent == "C":
+                arrayA = [0];
+                break;
+            case button.textContent == "Del":
+                arrayA.pop();
+                break;
+        }
+        updateDisplay(arrayA);
+    })
+});
+
+
+function addFunc (a, b) {
     return a + b;
 }
-function subtract (a, b) {
+function subtractFunc (a, b) {
     return a - b;
 }
-function multiply (a, b) {
+function multiplyFunc (a, b) {
     return a * b;
 }
-function divide (a, b) {
+function divideFunc (a, b) {
     return a / b;
 }
